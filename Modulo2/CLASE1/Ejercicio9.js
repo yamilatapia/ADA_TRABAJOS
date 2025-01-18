d19077577c77ac0a0d64193e7c6129630cf634aa
yamilatapia <yamilatapi@hotmail.com>
2025-01-18 11:49:32 -0300

    Utilizamos el objeto Set y el operador Spread

--- Modulo2/CLASE1/Ejercicio9.js
+++ Modulo2/CLASE1/Ejercicio9.js
@@ -1,6 +1,18 @@
-/* Filtrar palabras largas        
-Escribe una función que reciba un array de palabras y un número, y 
-devuelva las palabras que tienen más caracteres que el número dado. 
+/*Escribe una función que reciba un array de números y devuelva un nuevo 
+array sin números repetidos. 
 Consigna: 
-• Usa el método filter(). 
-• Usa una función flecha para simplificar el código. */
+• Usa el objeto Set para eliminar duplicados. 
+• Convierte el Set a un array usando el operador spread (...).*/
+
+let numeros = [1, 2, 3, 2, 4, 1, 5];
+
+let newNumerosSet = new Set(numeros); //eliminamos los numeros repetidos utilizando el objeto set
+
+//console.log(newNumerosSet);
+
+function numerosSinRepetir(numeros){
+    let newNumerosArray = [...newNumerosSet]; // convertimos el set a un array utilizando el operador spread ...
+    return newNumerosArray;
+} 
+
+console.log(numerosSinRepetir(numeros));
