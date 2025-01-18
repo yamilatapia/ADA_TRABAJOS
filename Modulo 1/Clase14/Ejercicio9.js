8608d5c957ba244861c4c1b0188051d531758a9c
yamilatapia <yamilatapi@hotmail.com>
2025-01-18 13:25:19 -0300

    Ultima aparicion de un modelo de auto

--- Modulo 1/Clase14/Ejercicio9.js
+++ Modulo 1/Clase14/Ejercicio9.js
@@ -10,4 +10,31 @@ Funcionalidad:
 ✓ Implementa la función ultimaAparicionModeloAuto(modelo), que toma 
 modelo como parámetro (un string). 
 ✓ Debes recorrer el array de manera manual para encontrar la última 
-aparición del modelo.*/
+aparición del modelo.
+ Si el modelo se encuentra en el array, la función debe imprimir por 
+consola la posición (índice + 1) en la que aparece (considerando que la 
+numeración es de 1 a N). 
+✓ Si el modelo no está en el array, debe imprimir un mensaje indicando que 
+el modelo no está presente. */
+
+
+let modelosAutos = ['Palio', 'Mobi', 'Cronos', 'Mobi', 'Pulse', 'Toro', 'Palio', 'Fiorino', 'Ducato']; 
+
+function ultimaAparicionModeloAuto(modelo){
+    let ultimaPosicion = -1; //Inicializamos en -1 para indicar que el modelo no se ha encontrado
+    for (let i = 0; i < modelosAutos.length; i++){
+        if(modelosAutos[i] === modelo){  //si encontramos el modelo
+            ultimaPosicion = i; //actualizamos con el indice de la ultima posicion
+        }
+    }
+    if (ultimaPosicion !== -1){
+        console.log(`El modelo ${modelo} aparece por ultima vez en la posicion ${ultimaPosicion + 1}`);
+        
+    } else {
+        console.log(`El modelo ${modelo} no se encuentra presente en la lista`)
+        
+    }
+}
+
+
+ultimaAparicionModeloAuto("Palio");
