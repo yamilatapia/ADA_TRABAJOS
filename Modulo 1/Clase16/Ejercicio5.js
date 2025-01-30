e41e4dcf3a12eefd2ba723f31a8cd5d686df031b
yamilatapia <yamilatapi@hotmail.com>
2025-01-29 21:13:57 -0300

    Utilizamos el metodo forEach

---
+++ Modulo 1/Clase16/Ejercicio5.js
@@ -0,0 +1,11 @@
+/*Recorre una lista e imprime cada elemento 
+Tienes una lista de frutas ['manzana', 'banana', 'cereza']. Tu tarea es 
+recorrer la lista y mostrar el nombre de cada fruta en la consola, una por 
+una.*/
+
+let frutas =  ['manzana', 'banana', 'cereza'];
+
+let listar = frutas.forEach(function(frutas){
+    console.log(frutas);
+    
+});
